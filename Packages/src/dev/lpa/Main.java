707039b166a0add537123e1d55798c2e504884bc
HoangTrong <hoangcongtrong054@gmail.com>
2023-01-16 16:39:50 +0700

    first commit

---
+++ Packages/src/dev/lpa/Main.java
@@ -0,0 +1,10 @@
+package dev.lpa;
+
+import com.abc.first.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Item fistItem = new Item("Burger");
+        System.out.println(fistItem);
+    }
+}
